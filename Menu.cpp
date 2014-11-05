366ad98dabb0aa1e9ef49d4f7a2277ba557187c6
Jose <you@example.com>
2014-11-05 17:25:26 +0100

    1ºVersion MENU

---
+++ Menu.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+ using namespace std;
+ int main(){
+ 	int opc;
+ 	do{
+ 		cout<<"------------------CLINICA--------------------\n";
+ 		cout<<"1.-Añadir\n";
+ 		cout<<"2.-Mostrar todos\n";
+ 		cout<<"3.-Buscar\n";
+ 		cout<<"4.-Modificar\n";
+ 		cout<<"5.-Eliminar\n";
+ 		cout<<"6.-Hacer copia de seguridad\n";
+ 		cout<<"7.-Restaurar copia\n";
+ 		cout<<"8.-Salir\n";
+ 		cout<<"Introduce la opcion:";
+ 		cin>>opc;
+ 		switch(opc){
+ 			case 1: {
+
+ 			}
+ 			case 2:{
+
+ 			}
+ 			case 3:{
+
+ 			}
+ 			case 4:{
+ 				
+ 			}
+ 			case 5:{
+ 				
+ 			}
+ 			case 6:{
+ 				
+ 			}
+ 			case 7:{
+ 				
+ 			}
+ 			case 8:{
+ 			
+ 			}
+ 		}
+ 	}while(opc!=8);
+
+
+ 	
+ 	return 0;
+ }
